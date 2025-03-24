f4f4b20e006c3c465a12eb1bf3d4d45bac4c962e
JarinKelder <jarinkelder@gmail.com>
2025-03-24 15:24:51 +0100

    Afmaken van de website

--- javascript/Portfolio.js
+++ javascript/Portfolio.js
@@ -1,25 +1,40 @@
 
 
   //initialize the map         
-  const map = L.map('leaflet').setView([52.527794, 5.434294], 15);
+  const map = L.map('leaflet').setView([52.35659612390365, 5.223194719579083], 17);
 
   //Create baselayer - tiles         
-  const backgroundMap = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',{
-    attribution: '<a href="http://openstreetmap.org">OpenStreetMap</a>contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+  const backgroundMap = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',{
+    attribution: '<a href="https://openstreetmap.org">OpenStreetMap</a>contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
     maxZoom: 19
     }
   );
-  
   backgroundMap.addTo(map);
+
+  // coordinaten polygoon school
+var latlngs = [
+  [52.35678540189037, 5.2229856752965],
+  [52.356710109770226, 5.223406722292131],
+  [52.35634407240467, 5.223218958091378],
+  [52.35641357340344, 5.222843429689868]
+];
+
+// polygoon school op kaart
+L.polygon(latlngs, {
+  color: '#e0b922',      
+  weight: 2,         
+  fillColor: '#113063', 
+  fillOpacity: 0.3  
+}).addTo(map);
 
 
   var maplibre = new maplibregl.Map({
     container: 'maplibre', // container id
     style: 'https://api.maptiler.com/maps/376f0fdc-7c5a-4c79-8a0a-85ea960874ce/style.json?key=XQDfTzE6Nuck7ThJ2kUb', // style URL
-    center: [0, 0], // starting position [lng, lat]
-    zoom: 1, // starting zoom
-    hash: true
+    center: [6.4524, 52.5103], // starting position [lng, lat]
+    zoom: 9, // starting zoom
 });
+
 
 maplibre.on('load', function(){
   console.log(maplibre.getStyle ())
@@ -58,7 +73,8 @@ fetch(url)
         'source': 'Ommen',
         'layout': {},
         'paint': {
-            'fill-color': '#000fff',
+            'fill-color': '#113063',
+            'fill-opacity': 0.3
         }
     });
     });
@@ -66,33 +82,3 @@ fetch(url)
   
   })
 
-
-// URL van de API om de geometrie van Zwolle op te halen
-var zwolleGeoUrl = "https://api.pdok.nl/bzk/locatieserver/search/v3_1/lookup?id=wpl-4b405aebf0e238eaff53612e5c6624e2&fl=*";
-
-// Controleer of de Leaflet-kaart correct is geïnitialiseerd
-map = document.querySelector(".leaflet")?.leafletInstance;
-
-if (!map) {
-    console.error("Leaflet-kaart voor Zwolle niet gevonden! Zorg dat de kaart correct is geïnitialiseerd.");
-} else {
-    fetch(zwolleGeoUrl)
-        .then(response => response.json())
-        .then(data => {
-            // Haal de WKT-geometrie van Zwolle op uit de API-response
-            var zwolleWKT = data.response.docs[0].geometrie_ll;
-
-            // Converteer WKT naar GeoJSON
-            var zwolleGeojson = Terraformer.wktToGeoJSON(zwolleWKT);
-
-            // Voeg de GeoJSON-data toe aan de Leaflet-kaart met een unieke laagnaam
-            L.geoJSON(zwolleGeojson, {
-                style: {
-                    color: "#ff0000", // Rode randkleur voor onderscheid
-                    weight: 2,
-                    fillOpacity: 0.3
-                }
-            }).addTo(map);
-        })
-        .catch(error => console.error("Fout bij het laden van GeoJSON voor Zwolle:", error));
-}
